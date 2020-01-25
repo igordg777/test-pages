d619677022e0453ec5ab9dbb9db4b40669e2a76b
Igor <igordg@mail.ru>
2020-01-25 22:26:32 +0300

    1.0.4

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ function test() {
         "    <meta name=\"viewport\"\n" +
         "          content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">\n" +
         "    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n" +
-        "<link rel=\"stylesheet\" href=\"index@.css\">"+
+        "<link rel=\"stylesheet\" href=\"index@.css\">" +
         "    <title>Document</title>\n" +
         "</head>\n" +
         "<body>\n" +
-        "<h6>Hello friend! Enjoy using! © iGorG, 2020<h6>"+
-        "<h6 align=\"center\">Test page<h6>"+
+        "<h6>Hello friend! Enjoy using! © iGorG, 2020<h6>" +
+        "<h6 align=\"center\">Test page<h6>" +
         "\n" +
         "\n" +
-        "<script index@.js></script>\n" +
-        "</body>\n" +
-        "</html>", function () {
-        console.log("The creation of your index@.html file   is complete!  завершено! © iGorG", '\n', "Создание файла index@.html завершено! © iGorG");
+        "<script src="index@.js"></script>\n" +
+            "</body>\n" +
+            "</html>", function () {
+                console.log("The creation of your index@.html file   is complete!  завершено! © iGorG", '\n', "Создание файла index@.html завершено! © iGorG");
 
-        fs.writeFile('index@.js', "", function () {
-            console.log("The creation of your index@.js file   is complete!  завершено! © iGorG", '\n', "Создание файла index@.js завершено! © iGorG");
-        });
+                fs.writeFile('index@.js', "", function () {
+                    console.log("The creation of your index@.js file   is complete!  завершено! © iGorG", '\n', "Создание файла index@.js завершено! © iGorG");
+                });
 
-        fs.writeFile('index@.css', "h6{\n" +
-            "opacity: 0.5;"+
-            "}", function () {
-            console.log("The creation of your index@.js file   is complete!  завершено! © iGorG", '\n', "Создание файла index@.css завершено! © iGorG");
-        });
-    });
+                fs.writeFile('index@.css', "h6{\n" +
+                    "opacity: 0.5;" +
+                    "}", function () {
+                        console.log("The creation of your index@.js file   is complete!  завершено! © iGorG", '\n', "Создание файла index@.css завершено! © iGorG");
+                    });
+            });
 }
 test()
